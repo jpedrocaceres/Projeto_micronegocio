fb75bb97da365839a2a688294e9235e648c855f7
jpedrocaceres <jpedro.goulart141@gmail.com>
2025-07-27 02:45:11 -0400

    atualizado

--- next.config.js
+++ next.config.js
@@ -8,5 +8,21 @@ const nextConfig = {
   },
   output: 'standalone',
 }
+const path = require('path');
 
-module.exports = nextConfig 
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+      '~': path.resolve(__dirname, 'src') // if using src directory
+    };
+    if (!isServer) {
+      config.mode = process.env.NODE_ENV || 'production';
+    }
+    if (process.env.NODE_ENV !== 'production') {
+      config.cache = false;
+    }
+    return config;
+  }
+};
